232291c59b5b160df0a808d9d15c42a38810cda7
Aravind <aravindgovindan001@gmail.com>
2023-01-11 11:29:56 -0700

    Add a dropdown component

---
+++ pages/components/dropdown.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Dropdown(props) {
+
+  const handleSelect = (option) => {
+    props.handleChange(option)
+
+  }
+
+  return (
+    <div className="dropdownContainer">
+      {props.label ? <label htmlFor="dropdown">{props.label}</label> : ''}
+      <select name="dropdown" value={props.selection} onChange={(e) => handleSelect(e.target.value)}>
+        {Object.keys(props.options).map(option => (
+          <option
+            key={props.options[option]}
+            value={option}
+          >
+            {props.options[option]}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
